f803f5506f2b2392fed500c493a3bda81ab60d33
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2022-06-20 02:18:36 -0700

    updating coloring/placing/more coding

--- script.js
+++ script.js
@@ -2,3 +2,35 @@ const h2 = document.createElement("h2");
 h2.textContent = "to my world!";
 
 document.querySelector("body").appendChild(h2);
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10);
+
+    if(left < 360){
+        dodger.style.left = `${left + 1}px`;
+
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
